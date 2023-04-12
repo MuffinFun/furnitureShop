90ee4184492d2775c089fac085bd5d474fcf2921
MaksimKriv <max.kriv3228@gmail.com>
2023-04-12 20:08:37 +0300

    top sec 90%

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,16 @@
 new Swiper('.real-slider', {
     slidesPerView: 1.4,
     spaceBetween: 32,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+      autoplay: {
+        delay: 3000
+      }
 });
